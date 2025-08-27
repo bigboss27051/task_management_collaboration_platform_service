e7799566d38843ce33a3a0a1c9912d51b38b1b29
bigoss27051 <bigboss27051@gmail.com>
2025-08-27 23:02:21 +0700

    init project

---
+++ src/main/java/org/tmcp/tmcp/TmcpApplication.java
@@ -0,0 +1,13 @@
+package org.tmcp.tmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmcpApplication.class, args);
+    }
+
+}
